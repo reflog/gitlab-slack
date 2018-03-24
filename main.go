bb81eb8c116dc2bf2e225d597733a8c191fe7e1d
Eli Yukelzon <reflog@gmail.com>
2018-03-24 18:00:02 +0300

    init

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ashwanthkumar/slack-go-webhook"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"gopkg.in/go-playground/webhooks.v3"
+	"gopkg.in/go-playground/webhooks.v3/gitlab"
+	"strconv"
+)
+
+var (
+	port       = kingpin.Flag("port", "Bind to this port").Short('p').Default("3016").Int()
+	endpoint   = kingpin.Flag("endpoint", "Webhook endpoint to receive events").Default("/webhook").Short('e').String()
+	webhookUrl = kingpin.Flag("webhookUrl", "Slack webhook to send events").Required().Short('s').String()
+	gitlabUrl  = kingpin.Flag("gitlabUrl", "Your Gitlab URL").Default("https://www.gitlab.com/").String()
+)
+
+func main() {
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+
+	hook := gitlab.New(&gitlab.Config{})
+	hook.RegisterEvents(HandlePipeline, gitlab.PipelineEvents)
+
+	err := webhooks.Run(hook, ":"+strconv.Itoa(*port), *endpoint)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// HandleRelease handles GitHub release events
+func HandlePipeline(payload interface{}, header webhooks.Header) {
+
+	fmt.Println("Handling Pipeline event")
+
+	pl := payload.(gitlab.PipelineEventPayload)
+	if pl.ObjectAttributes.Status != "success" {
+
+		attachment1 := slack.Attachment{}
+		attachment1.AddField(slack.Field{Title: "Failed commit", Value: fmt.Sprintf("<%s|link>", pl.Commit.URL)}).AddField(slack.Field{Title: "Author", Value: pl.Commit.Author.Name})
+		slackPayload := slack.Payload{
+			Text:        fmt.Sprintf("Hi! Project: *%s* failed to build! Here's a link to the <%s/%s/pipelines/%d|failed pipeline>", *gitlabUrl, pl.Project.PathWithNamespace, pl.Project.PathWithNamespace, pl.ObjectAttributes.ID),
+			Username:    "Gitlab",
+			Attachments: []slack.Attachment{attachment1},
+		}
+		err := slack.Send(*webhookUrl, "", slackPayload)
+		if len(err) > 0 {
+			fmt.Printf("error: %s\n", err)
+		}
+	}
+
+}
